ffb9593c03810b1dc34ecd5657ef07a3a9c1ec56
Almerio Barros da Silva Filho <138242853+AlmerioBarros@users.noreply.github.com>
2024-06-26 15:05:51 -0300

    configurando cards

--- JS/Criando_cards.js
+++ JS/Criando_cards.js
@@ -5,7 +5,7 @@ export default function Criando_card(informações_sobre_o_card) {
     const container_dos_cards = document.getElementById('contem_cards')
 
     container_dos_cards.innerHTML = `
-        <div class="card">
+        <div class="card_p">
         <h1>${informações_sobre_o_card["pas_01"]["01"]["nome"]} </h1>
         </div>
     `
