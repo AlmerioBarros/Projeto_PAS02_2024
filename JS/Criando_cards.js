58c598135b9f3c95d267b1c299bcad1017c16f04
Almerio Barros da Silva Filho <138242853+AlmerioBarros@users.noreply.github.com>
2024-07-03 17:16:32 -0300

    Criando card dinamicamente pt1

--- JS/Criando_cards.js
+++ JS/Criando_cards.js
@@ -4,10 +4,27 @@ export default function Criando_card(informações_sobre_o_card) {
 
     const container_dos_cards = document.getElementById('contem_cards')
 
-    container_dos_cards.innerHTML = `
+    Object.keys(informações_sobre_o_card.pas_02).forEach(e => {
+        container_dos_cards.innerHTML += `
+        <div class="card_p">
+        <h1>${informações_sobre_o_card["pas_02"][e]["nome"]} </h1>
+        </div>
+    `
+    })
+
+    
+
+    console.log()
+
+    
+
+
+
+
+
+    /*container_dos_cards.innerHTML = `
         <div class="card_p">
         <h1>${informações_sobre_o_card["pas_01"]["01"]["nome"]} </h1>
         </div>
-    `
-
+    `*/
 }
