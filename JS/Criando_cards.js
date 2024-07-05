8845f395b50c392eedc74f0cac0642143708219a
Almerio Barros da Silva Filho <138242853+AlmerioBarros@users.noreply.github.com>
2024-07-04 22:19:36 -0300

    alterando fundo dos cards

--- JS/Criando_cards.js
+++ JS/Criando_cards.js
@@ -6,7 +6,7 @@ export default function Criando_card(informações_sobre_o_card) {
 
     Object.keys(informações_sobre_o_card.pas_02).forEach(e => {
         container_dos_cards.innerHTML += `
-        <div class="card_p">
+        <div class=" card  card_p " style="background-image: url(${informações_sobre_o_card["pas_02"][e]["imagem"]});">
             <h1>${informações_sobre_o_card["pas_02"][e]["nome"]} </h1>
             <img class="imagem_card" src="${informações_sobre_o_card["pas_02"][e]["imagem"]}" alt="Imagem Exemplo">
             <p>Autor: ${informações_sobre_o_card["pas_02"][e]["autor"]} </p>
@@ -14,17 +14,47 @@ export default function Criando_card(informações_sobre_o_card) {
             <p>Caracteristicas: ${informações_sobre_o_card["pas_02"][e]["caracteristicas"]} </p>
         </div>
     `
+
     })
 
-    const cards = document.querySelectorAll(".card_p")
+    const cards = document.querySelectorAll(".card")
 
     cards.forEach(e => {
-        e.addEventListener('click', () => TrocarClass(e) )
+
+        e.addEventListener('click', () => TrocarClass(e))
+        e.addEventListener('click', () => TrocarFundo(e))
+        
     })
+
 
 
     function TrocarClass(element){
         element.classList.toggle("card_p")
         element.classList.toggle("card_g")
+ 
     }
+
+    function TrocarFundo(element){
+        if (element.classList.contains("card_p")) {
+            let url_fundo = `${element.children[1].src}`
+            element.style.backgroundImage = ` url(${url_fundo})`
+            
+        } else if(element.classList.contains("card_g")) {
+            let url_fundo = ``
+            element.style.backgroundImage = ` url(${url_fundo})`
+            
+        }
+    }
+
 }
+
+
+
+/*            currentSrc: https://live.staticflickr.com/7145/6730647385_7a7975fd4e_b.jpg" */
+  /*   url ( "${e.children[1].src}")  */
+
+  /*        let url_fundo = `${element.children[1].src}`
+        element.style.background = ` url(${url_fundo})`                     */
+
+/*                const estilo_do_elemento = element.getAttribute("style")
+        console.log(teste)       */
